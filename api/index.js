b5b7f33cf9cf9a8c544b3bc14b30114e64095da2
thuydiem1411 <nguyendiem14112002@gmail.com>
2024-02-01 14:44:53 +0700

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -1,19 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
-import authRouter from "./routes/auth.route.js"
-import userRouter from "./routes/user.route.js"
+import dotenv from "dotenv";
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
-mongoose.connect(process.env.MONGO).then(()=>{
-  console.log('Conneted to MongoDB!');
-}).catch((err) => {
-  console.log(err);
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Conneted to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 app.use(express.json());
 app.listen(3000, () => {
   console.log("Sever is running on port 3000!");
 });
 
-app.use('/api/user', userRouter);
-app.use('/api/auth',authRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Intenal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
